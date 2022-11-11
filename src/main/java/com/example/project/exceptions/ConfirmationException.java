1906a37c486d4aa39b707877110234f4f0ef44da
Andrey <arkhonfills@list.ru>
2022-11-11 17:20:39 +0300

    Initial commit

---
+++ src/main/java/com/example/project/exceptions/ConfirmationException.java
@@ -0,0 +1,8 @@
+package com.example.project.exceptions;
+
+public class ConfirmationException extends Exception{
+
+    public ConfirmationException (String message) {
+        super(message);
+    }
+}
